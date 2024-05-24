91c2d73948d18c5ffb6104303684772c4238851e
Ogunsanya Taoeeq <taofeeq.muhammad22@gmail.com>
2024-05-24 14:30:29 +0100

    pagination feature added

--- 0x06-dashboard_project/assets/js/emp-script.js
+++ 0x06-dashboard_project/assets/js/emp-script.js
@@ -19,14 +19,13 @@ for (let index = 0; index < btnView.length; index++) {
 				gridView.style.display = "flex";
 				listView.style.display = "none";
 			}
-			else
-			{
+			else {
 				listView.style.display = "block";
 				gridView.style.display = "none";
 				btnView[i].classList.add("btn-alt");
 				btnView[i].setAttribute("status", "inactive");
 			}
-		}	
+		}
 	}
 }
 
@@ -39,16 +38,16 @@ termSearch.onkeyup = () => {
 	//filter value
 	let filterValue = termSearch.value;
 	let item = document.querySelectorAll('#terms .data');
-	
-	for(let i = 0; i < item.length; i++){
+
+	for (let i = 0; i < item.length; i++) {
 		let data = item[i].querySelectorAll('.data td')[1];
 		let empId = item[i].querySelectorAll('.data td')[2];
 		let role = item[i].querySelectorAll('.data td')[3];
 		let status = item[i].querySelectorAll('.data td')[4];
 		let teams = item[i].querySelectorAll('.data td')[5];
 
-		
-		if(data.innerHTML.indexOf(filterValue) > -1 || empId.innerHTML.indexOf(filterValue) > -1 || role.innerHTML.indexOf(filterValue) > -1 || status.innerHTML.indexOf(filterValue) > -1 || teams.innerHTML.indexOf(filterValue) > -1){
+
+		if (data.innerHTML.indexOf(filterValue) > -1 || empId.innerHTML.indexOf(filterValue) > -1 || role.innerHTML.indexOf(filterValue) > -1 || status.innerHTML.indexOf(filterValue) > -1 || teams.innerHTML.indexOf(filterValue) > -1) {
 			item[i].style.display = '';
 		} else {
 			item[i].style.display = 'none';
@@ -61,12 +60,12 @@ termSearch.onkeyup = () => {
 	//filter value
 	let filterValue = termSearch.value;
 	let item = document.querySelectorAll('#role-data .data');
-	for(let i = 0; i < item.length; i++){
+	for (let i = 0; i < item.length; i++) {
 		let position = item[i].querySelectorAll('.data > div')[0];
 		let status = item[i].querySelectorAll('.data > div')[1];
 		let team = item[i].querySelectorAll('.data > div')[2];
 		let data = item[i].querySelectorAll('.data > div')[3];
-		if(team.innerHTML.indexOf(filterValue) > -1 || data.innerHTML.indexOf(filterValue) > -1 || position.innerHTML.indexOf(filterValue) > -1 || status.innerHTML.indexOf(filterValue) > -1){
+		if (team.innerHTML.indexOf(filterValue) > -1 || data.innerHTML.indexOf(filterValue) > -1 || position.innerHTML.indexOf(filterValue) > -1 || status.innerHTML.indexOf(filterValue) > -1) {
 			item[i].style.display = '';
 		} else {
 			item[i].style.display = 'none';
@@ -77,7 +76,66 @@ termSearch.onkeyup = () => {
 
 
 
-//  addEventListener('keyup', filterNames);
+
+
+// PAGINATION 
+let currentPage = 1;
+let limit = 3;
+
+let list = document.querySelectorAll('#terms .data');
+
+
+function loadItem() {
+	let startGet = limit * (currentPage - 1);
+	let endGet = limit * currentPage - 1;
+	list.forEach((item, key) => {
+		if (key >= startGet && key <= endGet) {
+			item.style.display = 'flex';
+		}
+		else {
+			item.style.display = 'none';
+		}
+	});
+	listPage();
+}
+
+loadItem();
+
+
+function listPage() {
+	let count = Math.ceil(list.length / limit);
+	document.querySelector('.page-nums').innerHTML = '';
+
+	for (let i = 1; i <= count; i++) {
+		let newPage = document.createElement('li');
+		newPage.innerText = i;
+		if (i == currentPage) {
+			newPage.classList.add('current');
+		}
+		newPage.setAttribute('onclick', "changePage(" + i + ")");
+		document.querySelector('.page-nums').appendChild(newPage);
+	}
+
+
+	let prevBtn = document.querySelector("#prev");
+	let nextBtn = document.querySelector("#next");
+	if (currentPage != 1) {
+		prevBtn.onclick = () => {
+			changePage(currentPage - 1);
+		}
+	}
+	if (currentPage >= 1 && currentPage != count) {
+		nextBtn.onclick = () => {
+			changePage(currentPage + 1);
+		}
+	}
+}
+
+
+function changePage(i) {
+	currentPage = i;
+	loadItem();
+}
 
 
 
