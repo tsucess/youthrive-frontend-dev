589945005ba0a247abbdc02cfd6ca4ddb4728e2a
tsuccess <taofeeq.muhammad22@gmail.com>
2024-06-17 01:36:30 +0100

    updated

--- 0x0B-eCommerce_Homepage_Figma_Design/src/components/hero/Hero.jsx
+++ 0x0B-eCommerce_Homepage_Figma_Design/src/components/hero/Hero.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-	<section className="container-fluid container_hero">
-		dfg
-	</section>
-  )
-}
+    <section className="container-fluid container_hero">
+      <div className="row">
+        <div className="col-12 col-md-6"></div>
+        <div className="col-12 col-md-6">
+          <div className="hero_info">
+            <p>New Arrival</p>
+            <h1>Discover Our New Collection</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+              tellus, luctus nec ullamcorper mattis.
+            </p>
+            <button type="button" className="btn btn-lg btn_hero mt-3">
+              BUY Now
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
